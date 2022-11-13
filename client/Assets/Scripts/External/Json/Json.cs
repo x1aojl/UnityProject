7720176065d047f912a7399a6cea2faba984d1d3
x1aojl <x1aojl@icloud.com>
2022-11-13 21:32:38 +0800

    提交Json库

---
+++ client/Assets/Scripts/External/Json/Json.cs
@@ -0,0 +1,29 @@
+﻿// Json.cs
+// Created by liujj2 Aug/28/2017
+// Json接口
+
+using JsonFx.Json;
+using System.Text;
+
+public class Json
+{
+    public static string SaveTo(object obj)
+    {
+        var stream   = new StringBuilder();
+        var settings = new JsonWriterSettings();
+
+        settings.PrettyPrint = true;
+        settings.Tab         = "    ";
+        settings.NewLine     = "\n";
+
+        var Writer = new JsonWriter(stream, settings);
+        Writer.Write(obj);
+
+        return stream.ToString();
+    }
+
+    public static T RestoreFrom<T>(string content)
+    {
+        return JsonReader.Deserialize<T>(content);
+    }
+}
